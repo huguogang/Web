87ebcbd65996fdfee9f1d310cd1dc1023483552a
huguogang <huguogang@g.email.noreply>
2015-10-10 09:22:47 -0700

    test cases

--- jasmine_tests/proto_spec.js
+++ jasmine_tests/proto_spec.js
@@ -1,7 +1,7 @@
 /// <reference path="../typings/jasmine/jasmine.d.ts"/>
 /// <reference path="../js/proto.js"/>
 
-describe("proto", function() {
+describe("parseProto", function() {
   beforeEach(function() {
     // setup code here
   });
@@ -16,8 +16,40 @@ describe("proto", function() {
     expect(actual).toEqual(expected);
   });
   
-  it("should parse single value", function() {
+  it("should parse number", function() {
     var protoText = "age: 100";
+    var expected = {"age": 100};
+    
+    var actual = parseProto(protoText);
+
+    //demonstrates use of custom matcher
+    expect(actual).toEqual(expected);
+  });
+  
+  it("should parse string", function() {
+    var protoText = 'name: "string"';
+    var expected = {"name": "string"};
+    
+    var actual = parseProto(protoText);
+
+    //demonstrates use of custom matcher
+    expect(actual).toEqual(expected);
+  });
+  
+  it("should parse array", function() {
+    var protoText = 'books: "book1" \
+      books: "book2"';
+    var expected = {"books": ["book1", "book2"]};
+    
+    var actual = parseProto(protoText);
+
+    //demonstrates use of custom matcher
+    expect(actual).toEqual(expected);
+  });
+  
+  it("should ignore bad input", function() {
+    // the part after "," is ignored
+    var protoText = "age: 100, height: 8";
     var expected = {"age": 100};
     
     var actual = parseProto(protoText);
