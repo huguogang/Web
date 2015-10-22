15695aab5adc676ec234cfaffc89cfd142f9b48d
huguogang <huguogang@g.email.noreply>
2015-10-22 00:53:51 -0700

    bones for a vocabulary testing page

---
+++ js/TypingData.js
@@ -0,0 +1,11 @@
+var TypingData = {
+  Latin2Eng: [
+    {
+      "test": "canis",
+      "answers": ["dog", "hound"]
+    },
+    {
+      "test": "villa",
+      "answers": ["town", "farm", "village"]
+    }]
+};
