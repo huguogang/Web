fa7c068944e6f58c9f0a6bcc33c04ffd69e479f1
huguogang <huguogang@g.email.noreply>
2015-10-23 21:28:44 -0700

    looks good now

--- js/TypingData.js
+++ js/TypingData.js
@@ -9,7 +9,55 @@ var TypingData = {
       "answers": ["town", "farm", "village"]
     },
     {
+      "test": "est",
+      "answers": ["is"]
+    },
+    {
+      "test": "pater",
+      "answers": ["father"]
+    },
+    {
+      "test": "mater",
+      "answers": ["mother"]
+    },
+    {
+      "test": "filius",
+      "answers": ["son"]
+    },
+    {
+      "test": "servus",
+      "answers": ["slave"]
+    },
+    {
+      "test": "coquus",
+      "answers": ["cook"]
+    },
+    {
+      "test": "scribit",
+      "answers": ["is writing"]
+    },
+    {
+      "test": "sedet",
+      "answers": ["is sitting"]
+    },
+    {
+      "test": "bibit",
+      "answers": ["is drinking"]
+    },
+    {
+      "test": "laborat",
+      "answers": ["is working"]
+    },
+    {
+      "test": "dormit",
+      "answers": ["is sleeping"]
+    },
+
+  ],
+  Chinese2Eng: [
+    {
       "test": "山",
       "answers": ["mountain", "hill"]
-    }]
+    }
+  ]
 };
