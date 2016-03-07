cf2481f4347a409cb1ad81c6640f2397c57cdfc7
huguogang <huguogang@g.email.noreply>
2016-03-06 23:03:45 -0800

    basic router implementation

--- ng2-ts/app/app.component.ts
+++ ng2-ts/app/app.component.ts
@@ -1,7 +1,36 @@
 import {Component} from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import {DeviceComponent} from "./device.component"
+import {ReportComponent} from "./report.component"
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `
+  <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink] = "['Device']">Device</a>
+      <a [routerLink] = "['Report']">Report</a>
+    </nav>
+    <router-outlet></router-outlet>`,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+     ROUTER_PROVIDERS
+    ]
 })
-export class AppComponent {}
+@RouteConfig([
+  {
+    path: '/device',
+    name: "Device",
+    component: DeviceComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/report',
+    name: 'Report',
+    component: ReportComponent
+  }
+])
+export class AppComponent {
+  title = 'Title';
+}
---
+++ ng2-ts/app/device.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-device',
+    template: '<h1>Device</h1>'
+})
+export class DeviceComponent { }
